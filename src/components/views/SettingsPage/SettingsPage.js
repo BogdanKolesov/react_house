3f389315585c222420f267147e18b05ef9b14cfa
Bogdan <bogdan_kolesov@bk.ru>
2021-08-28 14:39:44 +0600

    add: SettingPage

---
+++ src/components/views/SettingsPage/SettingsPage.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import React from 'react';
+import { FiSettings } from 'react-icons/fa';
+
+const SettingsPage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SettingsPage;
